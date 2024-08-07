c456c4065e77d2c0e4d9ceb00ca62ed7ee38f09c
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-08-08 00:36:01 +0530

    Use threads instead of processes

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "backlight.h"
 #include "mode.h"
 #include "config.h"
+
+void *mode_switch_thread(void *arg);
+void *backlight_control_thread(void *arg);
 
 int main() {
     char mode[256];
@@ -12,16 +15,31 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork error");
+    pthread_t mode_thread, backlight_thread;
+
+    if (pthread_create(&mode_thread, NULL, mode_switch_thread, (void *)mode) != 0) {
+        perror("Error creating mode switch thread");
         return EXIT_FAILURE;
-    } else if (pid == 0) {
-        // Child process: Handle mode switching
-        handle_mode_switch(mode);
-    } else {
-        backlight();
     }
+
+    if (pthread_create(&backlight_thread, NULL, backlight_control_thread, NULL) != 0) {
+        perror("Error creating backlight thread");
+        return EXIT_FAILURE;
+    }
+
+    pthread_join(mode_thread, NULL);
+    pthread_join(backlight_thread, NULL);
+
     return EXIT_SUCCESS;
 }
 
+void *mode_switch_thread(void *arg) {
+    char *mode = (char *)arg;
+    handle_mode_switch(mode);
+    return NULL;
+}
+
+void *backlight_control_thread(void *arg) {
+    backlight();
+    return NULL;
+}
